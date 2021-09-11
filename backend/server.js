f847e37ba657201145456ddaf94232f0878d1de1
Suresh Thapa <iamtechnicalsuresh@gmail.com>
2021-09-11 18:51:41 +0530

    Edit Pages added

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import path from "path";
 import connectDB from "./configs/db.js";
 import globleErrorHandler from "./middlewares/globleErrorHandler.js";
 
@@ -21,12 +21,22 @@ app.use("/api/v1/courses", coursesRoutes);
 app.use("/api/v1/contact", contactUsRoute);
 app.use("/api/v1/users", userRoutes);
 
-app.get("/", (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "Router working fine",
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
-});
+} else {
+  app.get("/", (req, res, next) => {
+    res.json({
+      status: "success",
+      message: "It's Working",
+    });
+  });
+}
 
 app.use(globleErrorHandler);
 
